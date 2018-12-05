14ccce749323a53fea4e9a29a832d54214de08f2
raymondelric <cw846@cornell.edu>
2018-12-04 23:00:43 -0500

    add timer

--- src/Main.java
+++ src/Main.java
@@ -12,22 +12,35 @@ public class Main{
 			if(args[0].equals("random"))	solver = new RandomSolver();
 			if(args[0].equals("minimax"))	solver = new MinimaxSolver();
 			if(args[0].equals("dfs"))		solver = new DFSSolver();
+			if(args[0].equals("heuristic"))	solver = new HeuristicSolver();
 		}
 		
-		TestData testdata = new TestData(4,200);
-		// generate 200 unique 4-digit string(lowercase alphabet only)
+		int count = (args.length > 1) ? Integer.parseInt(args[1]) : 1;
 
-		System.out.println(testdata.getList());
-		// print out the generated list
+		long startTime = System.nanoTime();
 
-		Master master = new Master(testdata.getSecret());
-		// randomly select a key
+		for(int i = 0;i < count;i++){
 
-		System.out.println(solver.findSecretWord(testdata.getList(), master));
-		// print out the # of guess
+			TestData testdata = new TestData(4,200);
+			// generate 200 unique 4-digit string(lowercase alphabet only)
 
-		solver.showGuessList();
-		// print out the every guess(the last is the answer)
+			// System.out.println(testdata.getList());
+			// print out the generated list
+
+			Master master = new Master(testdata.getSecret());
+			// randomly select a key
+
+			System.out.println(solver.findSecretWord(testdata.getList(), master));
+			// print out the # of guess
+
+			// solver.showGuessList();
+			// print out the every guess(the last is the answer)
+
+		}
+
+		long endTime   = System.nanoTime();
+		double totalTime = (double)(endTime - startTime) / 1_000_000_000;
+		System.out.printf("%.3f\n", totalTime);
 
 		return;
 	}
